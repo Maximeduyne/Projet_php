f17fe58e3874dae681d425e3acf305d4dd6fa0ec
maximeduyne <maximeduyne@icloud.com>
2022-01-07 13:48:21 +0100

    modification finale

---
+++ create_operation.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
+    <title>Create Account</title>
+</head>
+
+<body>
+
+<?php
+
+include 'librairie.php';
+CreateOperation();
+
+?>
+
+<a class="btnPage" href="index.php">Retour à mes comptes</a>
+
+</body>
+</html>
+
